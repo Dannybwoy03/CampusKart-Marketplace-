ff7df1092c4c8be44ad38e0f325b55f73b72e422
Daniel <campuskart03@gmail.com>
2025-09-03 21:24:10 +0000

    Fix Prisma schema location for Railway deployment

--- server/prisma.js
+++ server/prisma.js
@@ -1,13 +1,19 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import { PrismaClient } from "../node_modules/@prisma/client/index.js";
+import { PrismaClient } from "@prisma/client";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const envPath = path.join(__dirname, "..", ".env");
 dotenv.config({ path: envPath });
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
